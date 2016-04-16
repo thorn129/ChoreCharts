8efa5e87182bf9e1c82474d4c739db55532efd79
Kyle <thorn129@mail.chapman.edu>
2016-04-16 13:50:21 -0700

    Initial Commit

---
+++ Chore Charts/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Chore Charts
+//
+//  Created by Kyle Thornton on 4/16/16.
+//  Copyright © 2016 ChrisAndKyle. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Chore Charts/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Chore Charts
+//
+//  Created by Kyle Thornton on 4/16/16.
+//  Copyright © 2016 ChrisAndKyle. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
